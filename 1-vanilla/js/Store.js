0ecbbcf663a6520eb396d53505d89954f328b889
cold_hailstone <cksn1993@gmail.com>
2021-08-06 15:25:25 +0900

    [순수JS 1] 폴더 구조

---
+++ 1-vanilla/js/Store.js
@@ -0,0 +1,11 @@
+const tag = "[store]";
+
+export default class Store {
+    constructor(storage) {
+        console.log(tag);
+
+        if (!storage) throw "no storage";
+
+        this.storage = storage
+    }
+}
