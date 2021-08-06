801872185c0cf989ce68cc9fdfcf0af2239911ec
cold_hailstone <cksn1993@gmail.com>
2021-08-06 15:54:53 +0900

    [순수JS 1] 검색폼 2

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,15 +1,27 @@
-import { qs } from '../helpers.js';
+import { qs, on } from '../helpers.js';
 import View from "./View.js";
 
 export default class SearchFormView extends View {
     constructor() {
         super(qs('#search-form-view'));
 
+        this.inputEelement = qs('[type=text]', this.element);
         this.resetElement = qs('[type=reset]', this.element);
+
         this.showResetButton(false);
+        this.bindEvent();
     }
 
     showResetButton(visible = true) {
         this.resetElement.style.display = visible ? 'block' : 'none';
+    }
+
+    bindEvent() {
+        on(this.inputEelement, 'keyup', () => this.handleKeyup());
+    }
+
+    handleKeyup() {
+        const { value } = this.inputEelement;
+        this.showResetButton(value.length > 0);
     }
 }
