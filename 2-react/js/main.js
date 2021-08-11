dd979cfa3a551fbe6b55878b97e1eec47a2782ad
cold_hailstone <cksn1993@gmail.com>
2021-08-11 16:59:25 +0900

    [검색폼 2] 폼 초기화

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -7,18 +7,25 @@ class App extends React.Component {
     };
   }
 
-  handleChangeInput(e) {
-    // this.state.searchKeyword = e.target.value;
-    // this.forceUpdate();
-
-    this.setState({
-      searchKeyword: e.target.value
-    });
-  }
-
   handleSubmit(e) {
     e.preventDefault();
     console.log(this.state.searchKeyword);
+  }
+
+  handleReset() {
+    this.setState({ searchKeyword: '' }, () => {
+      console.log(this.state.searchKeyword);
+    });
+  }
+
+  handleChangeInput(e) {
+    const searchKeyword = e.target.value;
+
+    if (!searchKeyword) {
+      return this.handleReset();
+    }
+
+    this.setState({ searchKeyword: searchKeyword });
   }
 
   render() {
@@ -33,7 +40,7 @@ class App extends React.Component {
           <h2 className="container">검색</h2>
         </header>
         <div className="container">
-          <form onSubmit={this.handleSubmit.bind(this)}>
+          <form onSubmit={this.handleSubmit.bind(this)} onReset={this.handleReset.bind(this)}>
             <input type="text" placeholder="검색어를 입력하세요" autoFocus value={this.state.searchKeyword} onChange={this.handleChangeInput.bind(this)} />
             {this.state.searchKeyword && (<button type="reset" className="btn-reset"></button>)}
           </form>
