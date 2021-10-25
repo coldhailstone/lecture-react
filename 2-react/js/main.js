ac303c2374409bf6128601a25429bc24925f6ac6
cold_hailstone <cksn1993@gmail.com>
2021-10-25 10:25:12 +0900

    [탭] 탭 보여주기

--- 2-react/js/main.js
+++ 2-react/js/main.js
@@ -1,5 +1,15 @@
 import store from './js/store.js';
 
+const TabType = {
+  KEYWORD: 'KEYWORD',
+  HISTORY: 'HISTORY'
+}
+
+const TabLabel = {
+  [TabType.KEYWORD]: '추천 검색어',
+  [TabType.HISTORY]: '최근 검색어',
+}
+
 class App extends React.Component {
   constructor() {
     super();
@@ -75,6 +85,15 @@ class App extends React.Component {
       <div className="empty-box">검색 결과가 없습니다</div>
     );
 
+    // TODO:
+    const tabs = (
+      <ul className="tabs">
+        {Object.values(TabType).map(tabType => {
+          return <li key={tabType}>{TabLabel[tabType]}</li>;
+        })}
+      </ul>
+    )
+
     return (
       <>
         <header>
@@ -83,7 +102,7 @@ class App extends React.Component {
         <div className="container">
           {searchForm}
           <div className="content">
-            {this.state.submitted && searchResult}
+            {this.state.submitted ? searchResult : tabs}
           </div>
         </div>
       </>
