41d63b951e5d38d307e32c64da8aa16efe96255a
cold_hailstone <cksn1993@gmail.com>
2021-10-26 16:45:55 +0900

    [구현하기 2] 조합: 컴포넌트 담기 2

--- 3-component/src/components/HistoryList.js
+++ 3-component/src/components/HistoryList.js
@@ -1,32 +1,48 @@
-// import React from 'react';
-// import List from "./List";
-// import store from '../Store';
-// import { formatRelativeDate } from '../helpers';
+import React from 'react';
+import List from "./List";
+import store from '../Store';
+import { formatRelativeDate } from '../helpers';
 
-// export default class HistoryList extends List {
-//     componentDidMount() {
-//         this.fetch();
-//     }
+export default class HistoryList extends React.Component {
+    constructor() {
+        super();
 
-//     fetch() {
-//         const data = store.getHistoryList();
-//         this.setState({ data });
-//     }
+        this.state = {
+            historyList: []
+        };
+    }
+    
+    componentDidMount() {
+        this.fetch();
+    }
 
-//     handleClickRemoveHistory(e, keyword) {
-//         e.stopPropagation();
+    fetch() {
+        const historyList = store.getHistoryList();
+        this.setState({ historyList });
+    }
 
-//         store.removeHistory(keyword);
-//         this.fetch();
-//     }
+    handleClickRemoveHistory(e, keyword) {
+        e.stopPropagation();
 
-//     renderItem({ keyword, date }) {
-//         return (
-//             <>
-//                 <span>{keyword}</span>
-//                 <span className="date">{formatRelativeDate(date)}</span>
-//                 <button className="btn-remove" onClick={(e) => this.handleClickRemoveHistory(e, keyword)}></button>
-//             </>
-//         )
-//     }
-// }
+        store.removeHistory(keyword);
+        this.fetch();
+    }
+
+    render() {
+        return (
+            <List 
+                data={this.state.historyList}
+                onClick={this.props.onClick}
+                renderItem={(item) => {
+                    return (
+                        <>
+                            <span>{item.keyword}</span>
+                            <span className="date">{formatRelativeDate(item.date)}</span>
+                            <button className="btn-remove" onClick={(e) => this.handleClickRemoveHistory(e, item.keyword)}></button>
+                        </>
+                    )
+                }}
+            />
+        )
+    }
+}
